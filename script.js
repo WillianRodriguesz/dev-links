623e65396c46d26917e820c0a4b25f5ff3762285
Willian Rodrigues <willianrdrigues@hotmail.com>
2023-06-16 12:06:59 -0300

    add readme

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  const img = document.querySelector("#profile img")
+  if (html.classList.toggle("light")) {
+    img.setAttribute("src", "./assets/AvatarClaro.png")
+  } else {
+    img.setAttribute("src", "./assets/AvatarEscuro.png")
+  }
 }
